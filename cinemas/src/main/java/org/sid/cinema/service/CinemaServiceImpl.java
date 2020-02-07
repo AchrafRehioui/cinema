4b6273bc2b1c268b2957040529158c5888672df9
HDA <HDA@LAPTOP-SKFG673F>
2020-02-07 19:54:53 +0100

    +add body of initPositions and initMoviestheater

--- cinemas/src/main/java/org/sid/cinema/service/CinemaServiceImpl.java
+++ cinemas/src/main/java/org/sid/cinema/service/CinemaServiceImpl.java
@@ -2,10 +2,17 @@ package org.sid.cinema.service;
 
 import java.util.stream.Stream;
 
+import org.sid.cinema.dao.CategoryRepository;
 import org.sid.cinema.dao.CinemaRepository;
 import org.sid.cinema.dao.CityRepository;
+import org.sid.cinema.dao.MoviestheaterRepository;
+import org.sid.cinema.dao.PositionRepository;
+import org.sid.cinema.dao.ProjectionRepository;
+import org.sid.cinema.dao.SessionRepository;
 import org.sid.cinema.entities.Cinema;
 import org.sid.cinema.entities.City;
+import org.sid.cinema.entities.Moviestheater;
+import org.sid.cinema.entities.Position;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,8 +21,25 @@ public class CinemaServiceImpl  implements ICinemaService{
 
 	@Autowired  //to add  dependency injection
 	private CityRepository cityRepository;
+	
 	@Autowired
 	private CinemaRepository cinemaRepository;
+	
+	@Autowired
+	private MoviestheaterRepository moviestheaterRepository;
+	
+	@Autowired
+	private PositionRepository positionRepository;
+	
+	@Autowired
+	private SessionRepository sessionRepository;
+	
+	@Autowired
+	private ProjectionRepository projectionRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	@Override
 	public void initCities() {
 		// TODO Auto-generated method stub
@@ -34,6 +58,7 @@ public class CinemaServiceImpl  implements ICinemaService{
 			.forEach(nameCinema->{
 				Cinema cinema=new Cinema ();
 				cinema.setName(nameCinema);
+				cinema.setNumberMoviestheater(3+(int)(Math.random()*7));
 				cinema.setCity(v);
 			});
 		});
@@ -42,13 +67,29 @@ public class CinemaServiceImpl  implements ICinemaService{
 	@Override
 	public void initMoviestheater() {
 		// TODO Auto-generated method stub
-		
+		cinemaRepository.findAll().forEach(cinema->{
+			for(int i=0;i<cinema.getNumberMoviestheater();i++) {
+				Moviestheater moviestheater=new Moviestheater();
+				moviestheater.setName("moviestheater" + (i+1));
+				moviestheater.setCinema(cinema);
+				moviestheater.setNumberPlace(20+(int)(Math.random()*10));
+				moviestheaterRepository.save(moviestheater);
+			}
+		});
 	}
 
 	@Override
 	public void initPositions() {
 		// TODO Auto-generated method stub
-		
+		moviestheaterRepository.findAll().forEach(moviestheater->{
+		    for(int i=0; i<moviestheater.getNumberPlace();i++) {
+		    	Position position=new Position();
+		    	position.setSeatnumber(i+1);
+		    	position.setMoviestheater(moviestheater);
+		    	positionRepository.save(position);
+		    	
+		    }	
+		});
 	}
 
 	@Override
